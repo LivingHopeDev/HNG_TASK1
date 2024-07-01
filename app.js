915b6884dcd1e016bedf8e42312435ec0cfb75d7
adewobiadetayo1@gmail.com <adewobiadetayo1@gmail.com>
2024-07-01 12:53:57 +0100

    refactor

--- app.js
+++ app.js
@@ -10,12 +10,11 @@ app.get("/api/hello", async (req, res) => {
   const { visitor_name } = req.query;
   const ipAddress = req.ipInfo;
   const visitor_ip = ipAddress.ip.split(",")[0];
-  const locationInfo = await getUserCityInfo(visitor_ip); //102.88.62.83   57.128.168.243
-  console.log(visitor_ip);
+  const locationInfo = await getUserCityInfo(visitor_ip);
   res.json({
     client_ip: visitor_ip,
-    location: locationInfo.location.name,
-    greetings: `Hello, ${visitor_name}!, the temperature is ${locationInfo.current.temp_c} degrees Celcius in ${locationInfo.location.name}`,
+    location: locationInfo?.location?.name,
+    greeting: `Hello, ${visitor_name}!, the temperature is ${locationInfo?.current?.temp_c} degrees Celcius in ${locationInfo?.location?.name}`,
   });
 });
 
